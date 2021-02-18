22b50be78137e85a17b71276e8b0edb9f5948e90
yoshida0261 <yoshida261@gmail.com>
2021-02-19 01:44:12 +0900

    viewholder を追加

--- app/src/main/java/com/stah/toeic5/AudioListAdapter.kt
+++ app/src/main/java/com/stah/toeic5/AudioListAdapter.kt
@@ -1,20 +1,22 @@
 package com.stah.toeic5
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.stah.toeic5.databinding.ListAudioToeic5Binding
 
-class AudioListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+class AudioListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     sealed class AdapterItem {
         data class SampleItem(val data: String)
     }
 
-    //class SampleItemViewHolder(val binding: ListItemSampleBinding) :
-    //        RecyclerView.ViewHolder(binding.root)
-    //class AdapterListViewHolder(val binding: )
+    class AdapterListViewHolder(val binding: ListAudioToeic5Binding) : RecyclerView.ViewHolder(binding.root)
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-
+        return AdapterListViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.list_audio_toeic5, parent, false))
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
