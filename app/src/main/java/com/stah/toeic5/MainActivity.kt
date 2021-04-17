916130213b91e0679f0fa5336c7c4d33cc067717
yoshida0261 <yoshida261@gmail.com>
2021-04-18 00:57:59 +0900

    media playerで音出し

--- app/src/main/java/com/stah/toeic5/MainActivity.kt
+++ app/src/main/java/com/stah/toeic5/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.stah.toeic5
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.stah.toeic5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        binding.play.setOnClickListener {
+            var mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.t1pt2q7)
+            mediaPlayer?.start() // no need to call prepare(); create() does that for you
+        }
     }
 }
