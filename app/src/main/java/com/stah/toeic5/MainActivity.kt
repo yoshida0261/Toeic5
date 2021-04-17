583df865dfbf053d79a7361769bf86d904e4b9f3
yoshida0261 <yoshida261@gmail.com>
2021-04-18 01:48:13 +0900

    compose化

--- app/src/main/java/com/stah/toeic5/MainActivity.kt
+++ app/src/main/java/com/stah/toeic5/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.stah.toeic5
 
+import android.content.Context
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.databinding.DataBindingUtil
 import com.stah.toeic5.databinding.ActivityMainBinding
 
@@ -12,11 +18,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
 
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-
-        binding.play.setOnClickListener {
-            var mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.t1pt2q7)
-            mediaPlayer?.start() // no need to call prepare(); create() does that for you
+        setContent{
+            MyApp(this)
         }
+    }
+}
+
+@Composable
+fun MyApp(context:Context){
+    Surface() {
+        Button(onClick = {
+            var mediaPlayer: MediaPlayer? = MediaPlayer.create(context, R.raw.t1pt2q7)
+            mediaPlayer?.start() // no need to call prepare(); create() does that for you
+        }){
+            Text(text = "Play on Compose")
+        }
+
     }
 }
