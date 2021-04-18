ba0173abe8cba48f9af26ff7f95270c970d08cc2
yoshida0261 <yoshida261@gmail.com>
2021-04-18 11:30:58 +0900

    data追加

--- app/src/main/java/com/stah/toeic5/MainActivity.kt
+++ app/src/main/java/com/stah/toeic5/MainActivity.kt
@@ -6,10 +6,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.ListItem
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.databinding.DataBindingUtil
 import com.stah.toeic5.databinding.ActivityMainBinding
 
@@ -18,21 +31,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
 
-        setContent{
+        setContent {
             MyApp(this)
         }
     }
 }
 
 @Composable
-fun MyApp(context:Context){
+fun MyApp(context: Context) {
+    
+    Column() {
+
+        repeat(10){
+            Text(text = "test")
+        }
+    }
+    /*
     Surface() {
         Button(onClick = {
             var mediaPlayer: MediaPlayer? = MediaPlayer.create(context, R.raw.t1pt2q7)
             mediaPlayer?.start() // no need to call prepare(); create() does that for you
-        }){
+        }) {
             Text(text = "Play on Compose")
         }
 
     }
+    
+     */
 }
