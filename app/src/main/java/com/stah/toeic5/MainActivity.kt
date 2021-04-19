540ad4f3c1535a1680d195337b25b142b444669d
yoshida0261 <yoshida261@gmail.com>
2021-04-20 02:28:53 +0900

    screenごとにファイルをわけた

--- app/src/main/java/com/stah/toeic5/MainActivity.kt
+++ app/src/main/java/com/stah/toeic5/MainActivity.kt
@@ -4,25 +4,12 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import com.stah.toeic5.ui.theme.pink100
+import com.stah.toeic5.ui.screen.Part2Screen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         setContent {
             MyApp(this)
         }
@@ -34,34 +21,3 @@ fun MyApp(context: Context) {
     Part2Screen()
 }
 
-@Composable
-fun Part2Screen(names: List<String> = List(25) { "Test1 Part2 Q${(7 + it)}" }) {
-    Column() {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.toeic5), contentDescription = null,
-                modifier = Modifier.height(120.dp),
-                contentScale = ContentScale.Crop,
-            )
-        }
-        Column(modifier = Modifier.fillMaxHeight()) {
-            Spacer(Modifier.height(16.dp))
-            Column() {
-                NameList(names = names)
-            }
-        }
-    }
-}
-
-@Composable
-fun NameList(names: List<String>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(items = names) { name ->
-            Text(modifier = Modifier.padding(16.dp), text = name)
-            Divider(color = pink100)
-        }
-    }
-}
---
+++ app/src/main/java/com/stah/toeic5/ui/screen/Part2ListScreen.kt
@@ -0,0 +1,48 @@
+package com.stah.toeic5.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.stah.toeic5.R
+import com.stah.toeic5.ui.theme.pink100
+
+@Composable
+fun Part2Screen(names: List<String> = List(25) { "Test1 Part2 Q${(7 + it)}" }) {
+    Column() {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.toeic5), contentDescription = null,
+                modifier = Modifier.height(120.dp),
+                contentScale = ContentScale.Crop,
+            )
+        }
+        Column(modifier = Modifier.fillMaxHeight()) {
+            Spacer(Modifier.height(16.dp))
+            Column() {
+                NameList(names = names)
+            }
+        }
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+            Text(modifier = Modifier.padding(16.dp), text = name)
+            Divider(color = pink100)
+        }
+    }
+}
