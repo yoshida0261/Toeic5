dbb9210b92e13866c4e3fe2b92c7ddfe03739252
yoshida0261 <yoshida261@gmail.com>
2021-04-22 02:46:23 +0900

    戻る処理を追加

--- app/src/main/java/com/stah/toeic5/Part2Activity.kt
+++ app/src/main/java/com/stah/toeic5/Part2Activity.kt
@@ -3,6 +3,7 @@ package com.stah.toeic5
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
@@ -25,7 +26,18 @@ class Part2Activity : AppCompatActivity() {
         fun createPart2Intent(context: Context) =
             Intent(context, Part2Activity::class.java)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home->{
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
+
+
 
 @Composable
 fun Part2() {
